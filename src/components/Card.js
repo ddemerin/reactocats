4be044d0c47d4b851ebf87aad3870796e5c71e00
Daniel Demerin <ddemerin.code@gmail.com>
2020-07-22 12:00:20 -0400

    Change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import '.styles/Card.scss'
 
 const Card = () => {
   const [kittehs, setKittehs] = useState([{ authors: [] }])
