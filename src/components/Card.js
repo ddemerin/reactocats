0ae722b88636856314dca11ec6b840a6c724b511
Daniel Demerin <ddemerin.code@gmail.com>
2020-07-22 11:59:59 -0400

    Stopping here... making new repo for new version

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import '.styles/Card.scss'
 
 const Card = () => {
   const [kittehs, setKittehs] = useState([{ authors: [] }])
